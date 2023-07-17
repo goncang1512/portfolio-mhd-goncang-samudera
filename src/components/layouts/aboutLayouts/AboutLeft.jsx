61027beaf6f9dcfb711d01a034dc7b131bd61aa1
goncang1512 <samuderanst@gmail.com>
2023-07-17 15:07:20 +0700

    New Update 17/07/2023

--- src/components/layouts/aboutLayouts/AboutLeft.jsx
+++ src/components/layouts/aboutLayouts/AboutLeft.jsx
@@ -4,11 +4,11 @@ import { AiOutlineArrowRight } from "react-icons/ai";
 function AboutLeft() {
   return (
     <div className="img-content relative rounded-xl">
-      <div className="container-rotate flex justify-center items-center w-36">
+      <div className="container-rotate flex justify-center items-center w-44">
         <img
-          src="../../public/assets/png/text-frontend.png"
+          src="https://stefantopalovicdev.vercel.app/static/media/text2.3d5aa6ba2d0632bb4e0572631c3f9dc2.svg"
           alt=""
-          className="image-rotation w-40 p-2"
+          className="image-rotation"
         />
         <div className="icon-arrow text-black absolute">
           <a href="https://github.com/goncang1512/portfolio-mhd-goncang-samudera">
